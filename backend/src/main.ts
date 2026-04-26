84058b1f45d99c774fdd09ee45ee16bcf3cd07d5
kklimanskas <klimanskask@gmail.com>
2026-04-26 19:55:35 +0300

    added validationpipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppLogger } from './logger/logger.service';
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
   app.useLogger(app.get(AppLogger));
   app.enableCors();
-  
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(3000);
 }
