12b350f63e5041a297af520a9bd7aacbdd4453dd
kklimanskas <klimanskask@gmail.com>
2026-04-28 20:33:05 +0300

    added player schema to module and mongoose

--- backend/src/nba/nba.module.ts
+++ backend/src/nba/nba.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
 import { NbaController } from './nba.controller';
 import { NbaService } from './nba.service';
+import { Player, PlayerSchema } from './schemas/player.schema';
 
 @Module({
-  imports: [HttpModule],
+   imports: [
+    HttpModule,
+    MongooseModule.forFeature([{ name: Player.name, schema: PlayerSchema }]),
+  ],
   controllers: [NbaController],
   providers: [NbaService],
 })
