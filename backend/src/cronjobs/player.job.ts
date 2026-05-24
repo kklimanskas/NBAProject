a6120253d09b132f5708276d2965b004c04c472e
kklimanskas <klimanskask@gmail.com>
2026-05-24 21:27:10 +0300

    add syncPlayers to update players from API

---
+++ backend/src/cronjobs/player.job.ts
@@ -0,0 +1,23 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { PlayerService } from '../player/player.service';
+import { PaginationDto } from 'src/dto/pagination.dto';
+
+@Injectable()
+export class PlayerJob {
+  private readonly logger = new Logger(PlayerJob.name);
+
+  constructor(private readonly playerService: PlayerService) {}
+
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async syncPlayers() {
+    try {
+      this.logger.log('Starting player sync');
+      const query: PaginationDto = { page: 1, perPage: 25 };
+      await this.playerService.populateDatabaseWithPlayers(query);
+      this.logger.log('Player sync complete');
+    } catch (error) {
+      this.logger.error('Player sync failed', (error as Error).stack);
+    }
+  }
+}
