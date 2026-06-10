4a0d924d62cf878101b47072747991687b8b1460
kklimanskas <klimanskask@gmail.com>
2026-06-10 19:05:55 +0300

    added swagger to main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -8,6 +9,13 @@ async function bootstrap() {
   });
   app.enableCors();
 
+  const config = new DocumentBuilder()
+    .setTitle('NBA Project API')
+    .setDescription('NBA player and team management API. Part of a Full Stack application. Provides endpoints for managing players and teams, including fetching, updating, and soft deleting records. Also includes cron jobs for data synchronization with balldontlie API.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(3000);
