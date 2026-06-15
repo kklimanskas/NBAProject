4b1abbebd9414b9ec30bb5990ed9ef46c0c8c05a
kklimanskas <klimanskask@gmail.com>
2026-06-15 20:15:44 +0300

    fix naming

--- frontend/src/components/Player/PlayerDetail.tsx
+++ frontend/src/components/Player/PlayerDetail.tsx
@@ -23,10 +23,8 @@ import { Position } from "../../enums/index.enum";
 import { PlayerEditDialog, ConfirmDialog } from "./PlayerDialog";
 import type { UpdatePlayerPayload } from "../../types/nba";
 import { deletePlayer } from "../../api/playerApi";
-import {
-  useUpdatePlayer,
-  useDeletePlayer,
-} from "../../hooks/usePlayerMutations";
+import { useUpdatePlayer } from "../../hooks/useUpdatePlayer";
+import { useDeletePlayer } from "../../hooks/useDeletePlayer";
 
 const getPositionColor = (position: string) => {
   switch (position) {
